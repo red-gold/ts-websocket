848997bc47150685138e1c8ac0a34e896775c408
Amir Movahedi <amir.gholzam@live.com>
2019-12-15 16:46:35 +0700

    Add cors origin.
    Signed-off-by: Amir Movahedi <amir.gholzam@live.com>

--- server.js
+++ server.js
@@ -38,7 +38,14 @@ const app = express()
 const cors = require('cors')({ origin: "https://red-gold.o6s.io" })
 app.disable('x-powered-by')
 
-app.use(cors)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+// app.use(cors)
 app.use(cookieParser)
 const rawBodySaver = function (req, res, buf, encoding) {
 
