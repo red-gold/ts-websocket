6704d19b125f014318f685dab4c1d72e29c6eccc
Amir Movahedi <amir.gholzam@live.com>
2019-12-18 10:40:16 +0700

    Handle dispatch log.
    Signed-off-by: Amir Movahedi <amir.gholzam@live.com>

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ const rawBodySaver = function (req, res, buf, encoding) {
 
   if (buf && buf.length) {
     req.rawBody = buf.toString(encoding || 'utf8');
+  } else {
+    req.rawBody = ""
   }
 }
 
